acbc6b10c2ac5241b51d02b4bf010d8d20200789
pabloosorio <pablo.osorio@xmarts.com>
2018-06-13 18:31:24 -0500

    Personalizaciones de formatos para navegante

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Personalizaciones Navegante',
+    'summary': ' Personalizaciones navegante',
+
+    'description': """
+    - Personalizaciones navegante
+    """,
+    'author': "Xmarts, Pablo Osorio",
+    'website': "http://www.xmarts.com",
+    'depends': ['base'],
+    'data': [
+        'reports/report_invoice.xml'
+    ]
+}
---
+++ models/models.py
@@ -0,0 +1,4 @@
+## -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _, tools
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
